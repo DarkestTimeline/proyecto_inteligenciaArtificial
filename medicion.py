be6fd0b0ee916c7cefc9ba5c33efc4d2b4f18829
Sergio Vasquez <80869962+DarkestTimeline@users.noreply.github.com>
2022-04-24 13:00:38 -0600

    Add files via upload

--- medicion.py
+++ medicion.py
@@ -1,8 +1,4 @@
-# python object_size.py --image images/example_01.png --width 0.955
-# python object_size.py --image images/example_02.png --width 0.955
-# python object_size.py --image images/example_03.png --width 3.5
-
-# import the necessary packages
+# importacion de las librerias necesarias
 from scipy.spatial import distance as dist
 from imutils import perspective
 from imutils import contours
@@ -16,97 +12,91 @@ def midpoint(ptA, ptB):
     return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
 
-# construct the argument parse and parse the arguments
+# construccion y procesamiento de los argumentos que se insertaran desde la linea de comando
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
-                help="path to the input image")
+                help="path a la imagen de entrada")
 ap.add_argument("-w", "--width", type=float, required=True,
-                help="width of the left-most object in the image (in inches)")
+                help="ancho de el objeto a la izquieda de la imagen (en pulgadas)")
 args = vars(ap.parse_args())
 
-# load the image, convert it to grayscale, and blur it slightly
+# cargar la imagen, convertirla a escala de grises, y aplicar un poco de blur
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
-# perform edge detection, then perform a dilation + erosion to
-# close gaps in between object edges
+# realizar deteccion de bordes, despues aplicar dilatacion y erosion a la imagen
+# para reducir las distancias entre los bordes de los objetos
 edged = cv2.Canny(gray, 50, 100)
 edged = cv2.dilate(edged, None, iterations=1)
 edged = cv2.erode(edged, None, iterations=1)
 
-# find contours in the edge map
+# encontrar los contornos
 cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
                         cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
 
-# sort the contours from left-to-right and initialize the
-# 'pixels per metric' calibration variable
+# ordenar los contornos de izquierda a derecha e inicializar la variable de
+# 'pixels per metric' que es la variable de calibracion
 (cnts, _) = contours.sort_contours(cnts)
 pixelsPerMetric = None
 
-# loop over the contours individually
+# recorrer los contornos
 for c in cnts:
-    # if the contour is not sufficiently large, ignore it
+    # si es contorno no es lo suficientemente grande, se ignora
     if cv2.contourArea(c) < 100:
         continue
 
-    # compute the rotated bounding box of the contour
+    # indetificar las cajas que delimitan cada objeto
     orig = image.copy()
     box = cv2.minAreaRect(c)
     box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
     box = np.array(box, dtype="int")
 
-    # order the points in the contour such that they appear
-    # in top-left, top-right, bottom-right, and bottom-left
-    # order, then draw the outline of the rotated bounding
-    # box
+    # ordenar los puntos de los contornos de las cajas, y
+    # luego conectar cada punto para formar la caja
     box = perspective.order_points(box)
     cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
 
-    # loop over the original points and draw them
+    # recorrer cada punto para trazar las lineas
     for (x, y) in box:
         cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
 
-    # unpack the ordered bounding box, then compute the midpoint
-    # between the top-left and top-right coordinates, followed by
-    # the midpoint between bottom-left and bottom-right coordinates
+    # calcular el punto medio de los bordes, midpoint
     (tl, tr, br, bl) = box
+
     (tltrX, tltrY) = midpoint(tl, tr)
     (blbrX, blbrY) = midpoint(bl, br)
 
-    # compute the midpoint between the top-left and top-right points,
-    # followed by the midpoint between the top-righ and bottom-right
     (tlblX, tlblY) = midpoint(tl, bl)
     (trbrX, trbrY) = midpoint(tr, br)
 
-    # draw the midpoints on the image
+    # dibujar los puntos medios en la imagen
     cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
     cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
     cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
     cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
 
-    # draw lines between the midpoints
+    # dibujar las lineas entre los puntos medios
     cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
              (255, 0, 255), 2)
     cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
              (255, 0, 255), 2)
 
-    # compute the Euclidean distance between the midpoints
+    # calcular la distancia euclidiana entre los puntos medios
     dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
     dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
 
-    # if the pixels per metric has not been initialized, then
-    # compute it as the ratio of pixels to supplied metric
-    # (in this case, inches)
+    # si la variable de calibracion no ha sido inicializada,
+    # inicalizar, en pulagadas
     if pixelsPerMetric is None:
         pixelsPerMetric = dB / args["width"]
 
-    # compute the size of the object
+    # calcular las dimensiones de los objetos
     dimA = dA / pixelsPerMetric
     dimB = dB / pixelsPerMetric
 
-    # draw the object sizes on the image
+    # insertar el texto que indica las dimensiones de los objetos
     cv2.putText(orig, "{:.1f}in".format(dimA),
                 (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
                 0.65, (255, 0, 255), 2)
@@ -114,6 +104,6 @@ for c in cnts:
                 (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
                 0.65, (255, 0, 255), 2)
 
-    # show the output image
+    # mostrar la imagen final
     cv2.imshow("Image", orig)
     cv2.waitKey(0)
